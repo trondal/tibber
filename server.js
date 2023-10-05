6d837b061ef99d42c49e07fe13653924aacf1576
Trond Albinussen <trond.albinussen@blinq.no>
2023-10-05 11:25:51 +0200

    test7

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the world of Containers on Azure Cloud!6");
+  res.send("Welcome to the world of Containers on Azure Cloud!7");
 });
 
 app.listen(config.PORT, () => {
