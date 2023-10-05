48a4251cae01c8161cbef7d65e835d54967ef594
Trond Albinussen <trond.albinussen@blinq.no>
2023-10-05 10:42:46 +0200

    Dockerfile, port 80, works locally

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the world of Containers on Azure Cloud!2");
+  res.send("Welcome to the world of Containers on Azure Cloud!3");
 });
 
 app.listen(config.PORT, () => {
