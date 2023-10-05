1267341fb76ca8a5d5ee4fbb8ad445b29a0c6fdc
Trond Albinussen <trond.albinussen@blinq.no>
2023-10-05 10:35:36 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const config = require("./config");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the world of Containers on Azure Cloud!2");
+});
+
+app.listen(config.PORT, () => {
+  console.log("Application started successfully on port: " + config.PORT);
+});
